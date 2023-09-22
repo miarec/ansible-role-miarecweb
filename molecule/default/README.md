9b73135e7b70b84ae4b2fe427a6ea56b82445907
timothylytle <87677364+timothylytle@users.noreply.github.com>
2023-09-22 17:32:59 -0600

    Fix installation on Ubuntu 22.04
    + Add molecule tests

---
+++ molecule/default/README.md
@@ -0,0 +1,20 @@
+# Molecule test this role
+
+Run Molecule test
+```
+molecule test
+```
+
+Run test with variable example
+```
+MOLECULE_DISTRO=centos7 MOLECULE_MIARECWEB_VERSION=8.0.0.3909 molecule test
+```
+
+## Variables
+ - `MOLECULE_DISTRO` OS of docker container to test, default `ubuntu2204`
+    List of tested distros
+    - `ubuntu2204`
+    - `ubuntu2004`
+    - `centos7`
+ - `MOLECULE_MIARECWEB_VERSION` defines variable `miarecweb_version`, default `8.0.0.3911`
+ - `MOLECULE_MIARECWEB_SECRET` defines variabled `miarecweb_secret`, default `secret`
---
+++ molecule/default/tests/test_defaults.py
@@ -0,0 +1,62 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+miarecweb_version = os.environ.get('MIARECWEB_VERSION')
+
+
+def test_directories(host):
+
+    dirs = [
+        "/opt/miarecweb/releases/{}".format(miarecweb_version),
+        "/var/log/miarecweb",
+        "/var/log/miarecweb/celery"
+    ]
+    for dir in dirs:
+        d = host.file(dir)
+        assert d.is_directory
+        assert d.exists
+
+def test_files(host):
+    files = [
+        "/opt/miarecweb/releases/{}/production.ini".format(miarecweb_version),
+        "/etc/systemd/system/celerybeat.service",
+        "/etc/systemd/system/celeryd.service",
+        "/var/log/miarecweb/celery/beat.log",
+        "/var/log/miarecweb/celery/worker1.log"
+    ]
+
+    for file in files:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
+
+def test_service(host):
+    if host.system_info.distribution == "ubuntu":
+        services = [
+            "celeryd",
+            "celerybeat",
+            "apache2"
+        ]
+    if host.system_info.distribution == "centos":
+        services = [
+            "celeryd",
+            "celerybeat",
+            "httpd"
+        ]
+
+    for service in services:
+        s = host.service(service)
+        assert s.is_enabled
+        assert s.is_running
+
+def test_socket(host):
+    sockets = [
+        "tcp://0.0.0.0:80",
+        "tcp://0.0.0.0:443"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
