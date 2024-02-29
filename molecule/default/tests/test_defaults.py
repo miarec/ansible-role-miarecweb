598ea7ddf2bc3284f1ae9a4f85fda7f0b1bf8306
Timothy Lytle <timothy.lytle@miarec.com>
2024-02-29 16:13:40 -0700

    add additonal verify step to molecule test

--- molecule/default/tests/test_defaults.py
+++ molecule/default/tests/test_defaults.py
@@ -60,3 +60,6 @@ def test_socket(host):
     for socket in sockets:
         s = host.socket(socket)
         assert s.is_listening
+
+def test_script(host):
+    assert host.run("/opt/miarecweb/current/pyenv/bin/python -m miarecweb.scripts.create_root_user -u admin -p admin").rc == 0, "Miarecweb script failed to execute"
